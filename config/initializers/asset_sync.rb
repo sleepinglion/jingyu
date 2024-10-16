569c5be677bb0a88877c6572ce9f2004ecc70ed9
sleepinglion <admin@sleepinglion.pe.kr>
2024-10-17 01:25:02 +0900

    modified:   config/initializers/asset_sync.rb

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -8,22 +8,10 @@ if defined?(AssetSync)
     config.aws_secret_access_key = ENV['ORACLE_SECRET_KEY']
 
     # Oracle Object Storage의 S3 호환 API 엔드포인트
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = 'AWS'
     config.fog_directory = ENV['ORACLE_BUCKET']
     config.fog_region = ENV['ORACLE_REGION']
     config.fog_host = ENV['ORACLE_ENDPOINT'] # Oracle Object Storage S3 호환 API 엔드포인트
-
-    config.aws_access_key_id = ENV['ORACLE_FINGERPRINT']
-    config.aws_secret_access_key = ENV['ORACLE_CLOUD_SECRET_KEY']
-
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['ORACLE_ACCESS_KEY'],  # S3 호환 API의 Access Key
-      aws_secret_access_key: ENV['ORACLE_SECRET_KEY'],  # S3 호환 API의 Secret Key
-      region: ENV['ORACLE_REGION'],  # 리전 이름
-      endpoint: "https://#{ENV['ORACLE_NAMESPACE']}.compat.objectstorage.#{ENV['ORACLE_REGION']}.oraclecloud.com"
-    }
-
 
     # 해당 리소스에 대한 public-read 권한 부여 (필요시 설정)
     config.aws_acl = 'public-read'
