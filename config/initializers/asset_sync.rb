17cf8a27f8b0788f31d006f0c98105786be9dc73
sleepinglion <admin@sleepinglion.pe.kr>
2024-10-17 03:23:32 +0900

    modified:   config/initializers/asset_sync.rb

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -2,13 +2,6 @@ if defined?(AssetSync)
   AssetSync.configure do |config|
     config.fog_provider = 'fog/oracle'  # Oracle Cloud용 fog provider 설정
 
-    config.fog_credentials = {
-      provider:              'Oracle',  # Oracle Cloud 사용
-      oracle_storage_namespace: ENV['ORACLE_NAMESPACE'],  # 네임스페이스
-      oracle_access_key:    ENV['ORACLE_ACCESS_KEY'],  # API Access Key
-      oracle_secret_key:    ENV['ORACLE_SECRET_KEY'],  # API Secret Key
-      region:                ENV['ORACLE_REGION'],  # 리전
-    }
 
     config.fog_directory = ENV['ORACLE_BUCKET']  # 버킷 이름
     config.asset_host = "https://#{ENV['ORACLE_NAMESPACE']}.compat.objectstorage.#{ENV['ORACLE_REGION']}.oraclecloud.com/#{ENV['ORACLE_BUCKET']}"
