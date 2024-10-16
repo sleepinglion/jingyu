fc80d36675a1842090ef6af24ce6825a00fa73a7
sleepinglion <admin@sleepinglion.pe.kr>
2024-10-17 02:37:01 +0900

    modified:   config/initializers/asset_sync.rb 	modified:   config/initializers/carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'Oracle'  # 사용할 fog provider
+  config.fog_provider = 'fog/oracle'
   config.fog_credentials = {
-    provider: 'Oracle',               # Oracle Cloud를 사용함을 명시
+    provider: 'oracle',           # Oracle Cloud를 사용함을 명시
     oracle_username: ENV['ORACLE_USERNAME'], # Oracle Cloud 계정 정보
     oracle_password: ENV['ORACLE_PASSWORD'], # Oracle Cloud 계정 비밀번호
     oracle_domain: ENV['ORACLE_DOMAIN'],     # Oracle Cloud 도메인
@@ -12,7 +12,6 @@ CarrierWave.configure do |config|
     oracle_private_key_path: ENV['ORACLE_PRIVATE_KEY_PATH'], # API Key의 프라이빗 키 파일 경로
     oracle_bucket: ENV['ORACLE_BUCKET'], # 사용할 버킷 이름
   }
-
   config.fog_directory  = ENV['ORACLE_BUCKET']   # 업로드할 버킷 이름
   config.fog_public     = true                       # 파일이 공개인지 여부
   config.fog_attributes = { cache_control: "public, max-age=86400" } # 캐싱 옵션
