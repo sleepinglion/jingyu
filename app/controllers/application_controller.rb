05e1bf75688e39c90cf6ea38a9101040efbf2899
sl_notebook <admin@jedaeroweb.co.kr>
2026-03-15 21:23:20 +0900

    modified:   app/controllers/application_controller.rb 	modified:   bin/dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,9 @@ class ApplicationController < ActionController::Base
     @meta_keywords    = t(:meta_keywords)
     @meta_image       = t(:meta_image)
     @meta_url         = t(:meta_url)
+
+    @aside_blog_categories = BlogCategory.where(:enable=>true)
+    @tags = Blog.tag_counts_on(:tags, :limit => 20, :order => "taggings_count desc")
   end
 
   def prepare_default_meta_tags
