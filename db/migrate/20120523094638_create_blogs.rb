d87bd51f889051d6dd4989dc9ede20f79e681ea9
sleepinglion <admin@jedaeroweb.co.kr>
2026-04-07 00:47:38 +0900

    수정함:        app/views/blogs/show.html.erb 	수정함:        db/migrate/20120523094638_create_blogs.rb

--- db/migrate/20120523094638_create_blogs.rb
+++ db/migrate/20120523094638_create_blogs.rb
@@ -5,9 +5,8 @@ class CreateBlogs < ActiveRecord::Migration[6.0]
       t.references :blog_category, :null => false
       t.string :title, :null => false, :limit => 60
       t.string :description, :null => false, :limit => 255
-      t.integer :blog_comments_count, null: false, default: 0
-      t.integer :blog_pictures_count, null: false, default: 0
       t.integer :count, :null => false, :default => 0
+      t.integer :blog_pictures_count, null: false, default: 0
       t.boolean :is_html, :null => false, :default => false
       t.boolean :enable, :null => false, :default => true
       t.timestamps null: false
