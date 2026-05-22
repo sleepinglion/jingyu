5acce65349ab463df85fac5de7e19b9495a30251
sleepinglion <admin@jedaeroweb.co.kr>
2026-05-22 22:04:52 +0900

    수정함:        app/javascript/application.js 	수정함:        app/views/admin/galleries/_form.html.erb

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "rails-ujs"
+import "trix"
 import "bootstrap";
 import "tagcanvas";  // TagCanvas 로드
 import 'common'
